ab2cb143deaf4566a248d647fad41b7d8c5ad763
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2019-07-02 04:30:01 +0200

    Initial commit. We can fetch the mazes and move around them with assertions preventing us from breaking bounds or attempting to cross walls.

---
+++ main.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+from termcolor import colored
+import server_communication as sc
+
+def showmaze():
+	for location in sc.baremap:
+		print('            ', end = '')
+		for square in location:
+			if square == 'X':
+				print(colored('X ', 'red'), end = '')
+			elif square == ' ':
+				print(colored('O ', 'white'), end = '')
+			elif square == 'B':
+				print(colored('B ', 'green'), end = '')
+			else:
+				print(colored('A ', 'blue'), end = '')
+		print()
+
+class cell:
+	def __init__(self, value):
+		super(cell, self).__init__()
+		self.value = value
+		self.stepped_on = False
+
+	def mark_as_stepped(self):
+		self.stepped_on = True
+
+	def __str__(self):
+		return '({}, {})'.format(self.value, self.stepped_on)
+		
+class coordinate:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def __str__(self):
+		return '({}, {})'.format(self.x, self.y)
+		
+class maze:
+	def __init__(self, baremap):
+		# 			 ('X' 'A' 'B' '', PASSED_HERE_ALREADY)
+		self.map = [[cell(c) for c in row] for row in baremap]
+		self.cur = self.start_coordinates()
+		self.read_cur().mark_as_stepped()
+		self.size = len(baremap)
+		self.move_log = ''
+
+	def start_coordinates(self):
+		for y, row in enumerate(self.map):
+			for x, cell in enumerate(row):
+				if cell.value == 'A':
+					return coordinate(x + 1, y + 1)
+
+	def get_by_coordinates(self, coord):
+		try:
+			return self.map[coord.y - 1][coord.x - 1]
+		except IndexError:
+			return cell('L')
+
+	def read_cur(self):
+		return self.get_by_coordinates(self.cur)
+
+	def peek_n(self):
+		north = self.cur
+		north.y -= 1
+		return self.get_by_coordinates(north)
+
+	def peek_e(self):
+		east = self.cur
+		east.x += 1
+		return self.get_by_coordinates(east)
+
+	def peek_s(self):
+		south = self.cur
+		south.y += 1
+		return self.get_by_coordinates(south)
+
+	def peek_w(self):
+		west = self.cur
+		west.x -= 1
+		return self.get_by_coordinates(west)
+
+	def move_n(self):
+		self.move_log += 'N'
+		north = self.cur
+		north.y -= 1
+		value = self.get_by_coordinates(north).value
+		assert(value != 'X')
+		assert(value != 'L')
+		self.cur = north
+		self.read_cur().mark_as_stepped()
+
+	def move_e(self):
+		self.move_log += 'E'
+		east = self.cur
+		east.x += 1
+		value = self.get_by_coordinates(east).value
+		assert(value != 'X')
+		assert(value != 'L')
+		self.cur = east
+		self.read_cur().mark_as_stepped()
+
+	def move_s(self):
+		self.move_log += 'S'
+		south = self.cur
+		south.y += 1
+		value = self.get_by_coordinates(south).value
+		assert(value != 'X')
+		assert(value != 'L')
+		self.cur = south
+		self.read_cur().mark_as_stepped()
+
+	def move_w(self):
+		self.move_log += 'W'
+		west = self.cur
+		west.x -= 1
+		value = self.get_by_coordinates(west).value
+		assert(value != 'X')
+		assert(value != 'L')
+		self.cur = west
+		self.read_cur().mark_as_stepped()
+
+showmaze()
+m = maze(sc.baremap)
---
+++ server_communication.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+# Makes wrapping your head around it easier. As long as the code is kept flexible it will work with much bigger mazes too.
+url = 'https://api.noopschallenge.com/mazebot/random?minSize=10&maxSize=10'
+data = requests.get(url).json()
+baremap = data['map']
