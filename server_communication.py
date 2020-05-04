a2bad0e1e680c656db1d773e8c76029841a51afe
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-05-04 13:05:38 +0200

    Made it harder.

--- server_communication.py
+++ server_communication.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Makes wrapping your head around it easier. As long as the code is kept flexible it will work with much bigger mazes too.
-url = 'https://api.noopschallenge.com/mazebot/random?minSize=10&maxSize=10'
+url = 'https://api.noopschallenge.com/mazebot/random?minSize=20&maxSize=20'
 data = requests.get(url).json()
 baremap = data['map']
 starting_coordinates = data['startingPosition']
